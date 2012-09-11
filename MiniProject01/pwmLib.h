9831abca0100699b57a4c38de4968dc4e60e9bb4
Ross Hansen <hansenrl@rose-hulman.edu>
2012-09-11 03:26:18 +0000

    cleanup pt 2

---
+++ MiniProject01/pwmLib.h
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int set_pin_mux(int num, int letter);
+int set_pwm_value(int num, int letter, char * func, int value);
+int pwm_on(int num, int letter, int period_freq, int dutyCycle);
+
+int pwm_update_duty_cycle(int num, int letter, int dutyCycle);
+int pwm_update_frequency(int num, int letter, int period_freq);
+int pwm_stop(int num, int letter);
